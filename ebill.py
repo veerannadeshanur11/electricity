3fa0d1c9228cd68204b97be8c87807a275a43ded
veerannadeshanur11 <shreyasdeshanur34@gmail.com>
2025-10-06 17:19:19 +0530

    Create ebill.py

---
+++ ebill.py
@@ -0,0 +1,12 @@
+# Rate per unit
+rate_per_unit = 5
+
+# Ask user for units consumed
+units = int(input("Enter the number of units consumed: "))
+
+# Calculate total bill
+total_bill = units * rate_per_unit
+
+# Display the results
+print("Units Consumed:", units)
+print("Total Bill: ₹", total_bill)
